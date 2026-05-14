073353d4bcd009998520434fcc92738565ccc317
alsameercooke657-maker <alsameercooke657@gmail.com>
2026-05-14 19:04:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
 const intro = document.getElementById("intro");
 const gallery = document.getElementById("film-gallery");
-
 const startScreen = document.getElementById("startScreen");
-const countdown = document.getElementById("countdown");
-
 const number = document.getElementById("number");
 
 let count = 5;
 let interval;
 
-// Beep sound
 function beep() {
   const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-
   const osc = audioCtx.createOscillator();
   const gain = audioCtx.createGain();
 
   osc.type = "square";
   osc.frequency.value = 800;
-
   gain.gain.value = 0.1;
 
   osc.connect(gain);
@@ -28,43 +22,43 @@ function beep() {
   osc.stop(audioCtx.currentTime + 0.1);
 }
 
-// Start countdown
 function startCountdown() {
-
-  countdown.classList.remove("hidden");
-
   interval = setInterval(() => {
-
     number.textContent = count;
-
     beep();
 
     count--;
 
     if (count < 0) {
-
       clearInterval(interval);
-
       showGallery();
     }
-
   }, 1000);
 }
 
-// Show gallery after countdown
 function showGallery() {
-
   intro.classList.add("hidden");
-
   gallery.classList.remove("hidden");
 }
 
-// Click to begin
 startScreen.addEventListener("click", () => {
-
   startScreen.classList.add("hidden");
 
-  count = 5;
-
+  count = 5; // reset just in case
   startCountdown();
 });
+
+document.body.style.background = "white";
+setTimeout(() => {
+  document.body.style.background = "black";
+}, 50);
+
+function endIntro() {
+  clearInterval(interval);
+
+  // Hide intro completely
+  document.getElementById("intro").classList.add("hidden");
+
+  // Show gallery AFTER countdown
+  document.getElementById("film-gallery").classList.remove("hidden");
+}
